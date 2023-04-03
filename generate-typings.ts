3f09c8ed9b39a7b2a9ca4c4122aacd4036f61d47
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-04-03 07:48:51 +0200

    feat: finish article code

--- generate-typings.ts
+++ generate-typings.ts
@@ -4,5 +4,6 @@ import { join } from 'path';
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
     typePaths: ['./src/**/*.graphql'],
-    path: join(process.cwd(), 'src/graphql.ts')
+    path: join(process.cwd(), 'src/graphql.ts'),
+    watch: true
 });
